ccd99fcfaa7e0516be29d400dc38313c1d47de16
mhy12345 <maohanyang789@163.com>
2017-10-11 09:15:54 +0800

    update

--- maoyan/movie_info.py
+++ maoyan/movie_info.py
@@ -1,15 +1,23 @@
+#coding:utf-8
 import urllib
 import urllib.request
 from bs4 import BeautifulSoup
 import re
 from database import db
+import time
+import random
 
 def fetch_basic_info(mid):
     result = {'id':mid}
     url = 'http://piaofang.maoyan.com/movie/%d'%mid
     html = urllib.request.urlopen(url).read().decode('utf-8')
+    with open('caches/%d_bas.html'%mid,'w') as f:
+        f.write(html)
     soup = BeautifulSoup(html,'lxml')
-    #print(soup.title)
+    if re.search(r'抱歉，您的访问被我们识别为机器行为',html):
+        return "Banned"
+    if soup.title == None:
+        return "Not found"
     result['title'] = soup.title.string
     res = soup.find(attrs={'class':'rating-num'})
     if res:
@@ -30,16 +38,22 @@ def fetch_basic_info(mid):
             res3 = w.find(class_='info-sb-num')
             result['boxOffice_'+t1.group(1)+'_ratio'] = t1.group(2)
             result['boxOffice_'+t1.group(1)+'_num'] = "".join(map(lambda w:w.strip(),res3.strings))
+    #print(result)
     db.maoyan_info.save(result)
+    return "Success"
 
 
 def fetch_cel_info(mid):
     url = 'http://piaofang.maoyan.com/movie/%d/celebritylist'%mid
     result = {'id':mid}
     html = urllib.request.urlopen(url).read().decode('utf-8')
-    with open('cel.html','w') as f:
+    with open('caches/%d_cel.html'%mid,'w') as f:
         f.write(html)
     soup = BeautifulSoup(html,'lxml')
+    if re.search(r'抱歉，您的访问被我们识别为机器行为',html):
+        return "Banned"
+    if soup.title == None:
+        return "Not found"
     result['title'] = soup.title.string
     panels = soup.findAll(class_='panel-main')
     for panel in panels:
@@ -57,14 +71,21 @@ def fetch_cel_info(mid):
                 elist.append(item.string)
         result['elist'] = elist
         result['clist'] = clist
-        print(result)
+        #print(result)
         db.maoyan_cel_info.save(result)
+    return "Success"
 
 def fetch_com_info(mid):
     url = 'http://piaofang.maoyan.com/movie/%d/companylist'%mid
     result = {'id':mid}
     html = urllib.request.urlopen(url).read().decode('utf-8')
+    with open('caches/%d_com.html'%mid,'w') as f:
+        f.write(html)
     soup = BeautifulSoup(html,'lxml')
+    if re.search(r'抱歉，您的访问被我们识别为机器行为',html):
+        return "Banned"
+    if soup.title == None:
+        return "Not found"
     result['title'] = soup.title.string
     panels = soup.findAll(class_='panel-main')
     for panel in panels:
@@ -76,8 +97,9 @@ def fetch_com_info(mid):
             for item in items:
                 clist.append(item.string)
         result['list'] = clist
-        print(result)
+        #print(result)
         db.maoyan_com_info.save(result)
+    return "Success"
 
 def clean():
     db.maoyan_com_info.remove({})
@@ -86,11 +108,18 @@ def clean():
 
 
 if __name__ == "__main__":
-    mid = 0
+    mid = 1
     clean()
     while True:
-        mid += 1
-        fetch_basic_info(mid)
-        fetch_cel_info(mid)
-        fetch_com_info(mid)
+        def run(func,mid):
+            while True:
+                res = func(mid)
+                print(func.__name__,"(%d)"%mid,"...",res)
+                if res == "Success":
+                    break
+                time.sleep(random.randint(1,30))
+        run(fetch_basic_info,mid)
+        run(fetch_cel_info,mid)
+        run(fetch_com_info,mid)
         print(mid)
+        mid += 1
